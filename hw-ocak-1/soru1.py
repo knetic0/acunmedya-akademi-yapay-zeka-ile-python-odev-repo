f06358ebf1c7d9d2dcd3451d6d7a2c9949ccedd7
mehmet.solak <solakmehmet02@gmail.com>
2025-03-20 17:17:08 +0300

    ocak 1.hafta odev

---
+++ hw-ocak-1/soru1.py
@@ -0,0 +1,5 @@
+name = input("Adınızı giriniz: ")
+age = int(input("Yaşınızı giriniz: "))
+year = int(input("Doğum yılınızı giriniz: "))
+
+print(f"Merhaba {name}! {age} yaşındasın ve {year} yılında doğmuşsun.")
---
+++ hw-ocak-1/soru2.py
@@ -0,0 +1,12 @@
+number1 = int(input("Birinci sayıyı giriniz: "))
+number2 = int(input("İkinci sayıyı giriniz: "))
+
+summation = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+division = number1 / number2
+
+print(f"Toplam: {summation}")
+print(f"Fark: {subtraction}")
+print(f"Çarpım: {multiplication}")
+print(f"Bölüm: {division}")
